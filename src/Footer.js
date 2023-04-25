55b38e10f9b69c3968a0c17040367bff33f9646b
Adewale Fatoki <adewaleftk@gmail.com>
2023-04-25 20:51:06 +0100

    Little Lemon Website Updated

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from "./icons_assets/Logo.svg"
+
+function Footer() {
+  return (
+    <div>
+        <img src={Logo} alt="Footer Logo" />
+        Doormat Nav
+        Contact
+        Links
+    </div>
+  )
+}
+
+export default Footer
