4758bebed69b346ee93ef775c7b7f5b450a1d36e
Adewale Fatoki <adewaleftk@gmail.com>
2023-04-29 22:19:03 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import BookingPage from './components/BookingPage';
 import './App.css';
 
 function App() {
   return (
       <>
         <Nav />
+        <BookingPage />
         <Header />
         <Main />
         <Footer />
---
+++ src/components/BookingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookingForm from './BookingForm'
+
+function BookingPage() {
+  return (
+    <div>BookingPage
+        <BookingForm />
+    </div>
+  )
+}
+
+export default BookingPage
