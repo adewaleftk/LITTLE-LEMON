bd61d121e29c97079bdb178879e7be84133b8f27
Adewale Fatoki <adewaleftk@gmail.com>
2023-04-29 23:40:53 +0100

    Responsiveness Updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import BookingPage from './components/BookingPage';
+import { useReducer } from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom';
+import { AppProvider, ThemeProvider } from './context';
+import { Header, Footer } from './components';
 import './App.css';
+import { Home, BookingPage, ConfirmedBooking } from './pages';
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="bookings" element={<BookingPage />}>
+        <Route path="thank-you" element={<ConfirmedBooking />} />
+      </Route>
+    </Route>
+  )
+);
 
 function App() {
+  const initialAppState = {
+    previousLocation: ['/'],
+  };
+  const reducer = (state, { type, payload }) => {
+    switch (type) {
+      case 'setPreviousLocation': {
+        return {
+          // ...state,
+          // previousLocation: [...state.previousLocation]
+          //   .slice(0, 1)
+          //   .push(payload),
+        };
+      }
+
+      default:
+        break;
+    }
+  };
+  const [stateGlobal, dispatchGlobal] = useReducer(reducer, initialAppState);
   return (
-      <>
-        <Nav />
-        <BookingPage />
-        <Header />
-        <Main />
-        <Footer />
-      </>
+    <AppProvider value={{ stateGlobal, dispatchGlobal }}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
